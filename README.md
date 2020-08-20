165f6b41fb522578b7ef272c6c01c3d454c354e1
ShinteiMai <stevenhanselgo@gmail.com>
2020-08-20 23:22:22 +0700

    Added a little bit of error handling and README.md

---
+++ README.md
@@ -0,0 +1,15 @@
+# sp4m_b0t.py
+
+Spam ur friends with this th1ng
+
+Currently, this only work for "root" or sudo because the keyboard module from pip only allows root authorization to execute.
+
+## 1. Install dependencies
+```
+sudo pip install -r requirements.txt
+```
+
+## 2. Run the script
+```
+sudo python sp4m_b0t.py
+```
--- sp4m_b0t.py
+++ sp4m_b0t.py
@@ -1,3 +1,4 @@
+import sys
 import pyautogui
 import time
 import keyboard
@@ -7,15 +8,19 @@ print('made by sh1nt31m41\n\n')
 
 print('Press Enter to determine your mouse position\n')
 
-while True:
-    x, y = pyautogui.position()
+try:
+    while True:
+        x, y = pyautogui.position()
 
-    if keyboard.is_pressed('enter'):
-        break
+        if keyboard.is_pressed('enter'):
+            break
 
-    positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
-    print(positionStr, end='')
-    print('\b' * len(positionStr), end='', flush=True)
+        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+        print(positionStr, end='')
+        print('\b' * len(positionStr), end='', flush=True)
+
+except KeyboardInterrupt:
+    sys.exit()
 
 _ = input('') # clear stdinput
 
@@ -26,5 +31,6 @@ while count > 0:
     pyautogui.click(x, y)
     pyautogui.write(message)
     pyautogui.press('enter')
+
     time.sleep(1)
     count -= 1
